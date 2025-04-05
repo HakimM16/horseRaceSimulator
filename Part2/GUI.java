7d314c5751482b12ea269d4d60564cd8d6267898
HakimM16 <hakimmabike2@gmail.com>
2025-04-05 20:41:27 +0100

    Made intial commit to GUI-development, started off by making the JFrame

---
+++ Part2/GUI.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class GUI {
+    public static void main(String[] args) {
+        // Create a new JFrame
+
+        JFrame frame = new JFrame("GUI development"); 
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exits the program when the window is closed
+        frame.setSize(400, 400); // set the size of the window
+        frame.setVisible(true); // make the window visible
+    }
+}
