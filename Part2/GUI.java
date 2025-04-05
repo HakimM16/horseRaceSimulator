9caa3aae1ca57b590e9b263fc520315db03d81cb
HakimM16 <hakimmabike2@gmail.com>
2025-04-05 20:49:55 +0100

    Added a comment to GUI.java

--- Part2/GUI.java
+++ Part2/GUI.java
@@ -3,7 +3,8 @@ import javax.swing.*;
 public class GUI {
     public static void main(String[] args) {
         // Create a new JFrame
-
+        // planning:
+        // - create a custom class that extends JFrame for the GUI
         JFrame frame = new JFrame("GUI development"); 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exits the program when the window is closed
         frame.setSize(400, 400); // set the size of the window
